353d6af0ec945fdb56bf4c468dfa3115a2db2b8a
corinnekrych <corinnekrych@gmail.com>
2014-07-25 17:22:02 +0200

    Add AGHttpStub fwk using submodule

--- AeroGearHttpTests/AGSessionImplTests.swift
+++ AeroGearHttpTests/AGSessionImplTests.swift
@@ -18,17 +18,17 @@
 
 import XCTest
 import AeroGearHttp
+import AGURLSessionStubs
 
 class AGSessionImplTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        StubsManager.removeAllStubs()
     }
     // TODO
     func testGETWithWrongUrlFormat() {
