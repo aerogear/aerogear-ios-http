17a75eb16a52c86fa79031c37ae7fee069fa407b
Christos Vasilakis <cvasilak@gmail.com>
2015-01-12 14:11:23 +0200

    added integration tests target

---
+++ AeroGearHttpIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AeroGearHttpIntegration
+//
+//  Created by Christos Vasilakis on 1/12/15.
+//  Copyright (c) 2015 aerogear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
