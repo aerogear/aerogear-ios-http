2bb2e90fb87b7c86984143c6a4cd7291dc059368
corinne krych <corinnekrych@gmail.com>
2015-03-23 11:05:47 +0100

    Remove oauth2 too specific methods

--- AeroGearHttp/AuthzModule.swift
+++ AeroGearHttp/AuthzModule.swift
@@ -28,34 +28,6 @@ public protocol AuthzModule {
     :param: completionHandler A block object to be executed when the request operation finishes.
     */
     func requestAccess(completionHandler: (AnyObject?, NSError?) -> Void)
-
-    /**
-    Request an authorization code.
-    
-    :param: completionHandler A block object to be executed when the request operation finishes.
-    */
-    func requestAuthorizationCode(completionHandler: (AnyObject?, NSError?) -> Void)
-
-    /**
-    Exchange an authorization code for an access token.
-    
-    :param: completionHandler A block object to be executed when the request operation finishes.
-    */
-    func exchangeAuthorizationCodeForAccessToken(code: String, completionHandler: (AnyObject?, NSError?) -> Void)
-    
-    /**
-    Request to refresh an access token.
-    
-    :param: completionHandler A block object to be executed when the request operation finishes.
-    */
-    func refreshAccessToken(completionHandler: (AnyObject?, NSError?) -> Void)
-    
-    /**
-    Request to revoke access.
-    
-    :param: completionHandler A block object to be executed when the request operation finishes.
-    */
-    func revokeAccess(completionHandler: (AnyObject?, NSError?) -> Void)
     
     /**
     Return any authorization fields.
@@ -63,12 +35,5 @@ public protocol AuthzModule {
    :returns:  a dictionary filled with the authorization fields.
     */
     func authorizationFields() -> [String: String]?
-    
-    /**
-    Returns a boolean indicating whether authorization has been granted.
-    
-    :returns: true if authorized, false otherwise.
-    */
-    func isAuthorized() -> Bool
     
 }
